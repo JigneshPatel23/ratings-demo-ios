de14c68d9c5dac654d6965bdda6fb81e1150e97e
Jignesh Patel <p.jignesh23@gmail.com>
2018-06-09 16:36:53 +0530

    Initial Commit

---
+++ Ratings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by Jignesh Patel on 09/06/18.
+//  Copyright © 2018 Jignesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
