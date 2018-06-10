d7a86dde9b6951da7ea968a1f938e8a8b0a525f1
Jignesh Patel <p.jignesh23@gmail.com>
2018-06-11 00:20:39 +0530

    Added Game Picker Feature

--- Ratings/PlayerDetailsViewController.swift
+++ Ratings/PlayerDetailsViewController.swift
@@ -13,20 +13,51 @@ class PlayerDetailsViewController: UITableViewController {
     //MARK: Properties
     var player: Player?
     
+    var game: String = "Chess" {
+        didSet {
+            detailLabel.text = game
+        }
+    }
+    
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var detailLabel: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        print("init PlayerDetailsViewController")
+        super.init(coder: aDecoder)
+    }
+    
+    deinit {
+        print("deinit PlayerDetailsViewController")
+    }
     
     //MARK: Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "SavePlayerDetail",
             let playerName = nameTextField.text {
-                player = Player(name: playerName, game: "Chess", rating: 1)
+                player = Player(name: playerName, game: game, rating: 1)
+        }
+        
+        if segue.identifier == "PickGame",
+            let gamePickerViewController = segue.destination as? GamePickerViewController {
+            gamePickerViewController.selectedGame = game
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+//MARK : IBActions
+extension PlayerDetailsViewController {
+    
+    @IBAction func unwindWithSelectedGame(segue: UIStoryboardSegue){
+        if let gamePickerViewController = segue.source as? GamePickerViewController,
+            let selectedGame = gamePickerViewController.selectedGame{
+            game = selectedGame
+        }
     }
 }
 
