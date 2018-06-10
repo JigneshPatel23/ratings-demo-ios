01165b85f2a81c1d5ac9e4a7fec5d813dd9fd239
Jignesh Patel <p.jignesh23@gmail.com>
2018-06-10 23:21:31 +0530

    Added feature to add player with name

--- Ratings/PlayerDetailsViewController.swift
+++ Ratings/PlayerDetailsViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class PlayerDetailsViewController: UITableViewController {
     
+    //MARK: Properties
+    var player: Player?
+    
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var detailLabel: UILabel!
+    
+    //MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SavePlayerDetail",
+            let playerName = nameTextField.text {
+                player = Player(name: playerName, game: "Chess", rating: 1)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +31,7 @@ class PlayerDetailsViewController: UITableViewController {
 }
 
 extension PlayerDetailsViewController{
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 0{
             nameTextField.becomeFirstResponder()
--- Ratings/PlayersViewController.swift
+++ Ratings/PlayersViewController.swift
@@ -43,5 +43,16 @@ extension PlayersViewController{
     
     @IBAction func savePlayerDetail(_ segue: UIStoryboardSegue){
         
+        guard let playerDetailsViewController = segue.source as? PlayerDetailsViewController,
+         let player = playerDetailsViewController.player else {
+            return
+        }
+        
+        // add the new player to the players array
+        players.append(player)
+        
+        // update the tableview
+        let indexPath = IndexPath(row: players.count - 1, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
 }
